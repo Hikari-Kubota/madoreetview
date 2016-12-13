0017738dfaecf3a15dc6552188a14b04fc66d95b
窪田 光 <h-kubota@PC-MAC-h-kubota.local>
2016-12-13 18:14:59 +0900

    Add(js) test objects

--- js/init.js
+++ js/init.js
@@ -1,3 +1,12 @@
+// 部屋を辿るルートを表現するグラフ(JSON形式)
+var graphData = {
+	0: {
+		"id": 0,
+		"adjNode": {},
+		"coord": {"x": null, "y": null},
+	}
+};
+
 (function(){
-	alert(graph.nodeList);
+	alert(image.test);
 })();
--- js/madolib.js
+++ js/madolib.js
@@ -2,10 +2,22 @@ var graph = (function(){
 	var nodeList = [
 		1,2,3
 	];
-	var node = {}
+	var node = {
+		id: null,
+		adjNode: [], // 隣接ノード
+		coord: {x: 2, y: null},
+	}
 
 	return {
 		nodeList: nodeList,
 		node: node,
+	}
+})();
+
+var image = (function(){
+	var test = graph.node.coord.x;
+
+	return {
+		test: test,
 	}
 })();
